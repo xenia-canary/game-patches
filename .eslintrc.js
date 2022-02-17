8943b62867a8ce1797957ce7128fda9fe223f401
Margen67 <Margen67@users.noreply.github.com>
2022-02-17 13:03:20 -0800

    Add linting CI

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: [
+    'plugin:toml/standard'
+  ],
+  rules: {
+    'toml/indent': 'off',
+    'toml/padding-line-between-tables': 'off',
+    'toml/padding-line-between-tables': 'off',
+    'toml/spaced-comment': 'off',
+    'toml/tables-order': 'off',
+    'toml/no-mixed-type-in-array': 'error',
+    'eol-last': ["error", "always"],
+    'no-mixed-spaces-and-tabs': "error",
+    'no-multi-spaces': ["error", { ignoreEOLComments: true }],
+    'no-multiple-empty-lines': ["error", { "max": 1, "maxBOF": 0}],
+    'no-tabs': "error",
+    'no-trailing-spaces': "error",
+    'unicode-bom': "error",
+  }
+}

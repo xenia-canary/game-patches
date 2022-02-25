8b49e5e567e428fbba31e7114bbc1b36f39f4324
Margen67 <Margen67@users.noreply.github.com>
2022-02-24 22:06:05 -0800

    Add indent to lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,20 @@
 module.exports = {
-  extends: [
-    'plugin:toml/standard'
-  ],
-  rules: {
-    'toml/indent': 'off',
-    'toml/padding-line-between-tables': 'off',
-    'toml/spaced-comment': 'off',
-    'toml/tables-order': 'off',
-    'toml/no-mixed-type-in-array': 'error',
-    'eol-last': ['error', 'always'],
-    'no-mixed-spaces-and-tabs': 'error',
-    'no-multi-spaces': ['error', { ignoreEOLComments: true }],
-    'no-multiple-empty-lines': ['error', { max: 1, maxBOF: 0, maxEOF: 0}],
-    'no-tabs': 'error',
-    'no-trailing-spaces': 'error',
-    'unicode-bom': 'error'
-  }
+    extends: [
+        'plugin:toml/standard'
+    ],
+    rules: {
+        'toml/indent': ['error', 4, { subTables: 1, keyValuePairs: 1 }],
+        'toml/padding-line-between-tables': 'off',
+        'toml/spaced-comment': 'off',
+        'toml/tables-order': 'off',
+        'toml/no-mixed-type-in-array': 'error',
+        'eol-last': ['error', 'always'],
+        'indent': 'error',
+        'no-mixed-spaces-and-tabs': 'error',
+        'no-multi-spaces': ['error', { ignoreEOLComments: true }],
+        'no-multiple-empty-lines': ['error', { max: 1, maxBOF: 0, maxEOF: 0 }],
+        'no-tabs': 'error',
+        'no-trailing-spaces': 'error',
+        'unicode-bom': 'error'
+    }
 }

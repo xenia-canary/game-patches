caafdc9050e8cd517eded3a97c26a2334052c361
Margen67 <Margen67@users.noreply.github.com>
2022-02-24 20:15:56 -0800

    .eslintrc.js: Use single quotes

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,12 +8,12 @@ module.exports = {
     'toml/spaced-comment': 'off',
     'toml/tables-order': 'off',
     'toml/no-mixed-type-in-array': 'error',
-    'eol-last': ["error", "always"],
-    'no-mixed-spaces-and-tabs': "error",
-    'no-multi-spaces': ["error", { ignoreEOLComments: true }],
-    'no-multiple-empty-lines': ["error", { "max": 1, "maxBOF": 0, "maxEOF": 0}],
-    'no-tabs': "error",
-    'no-trailing-spaces': "error",
-    'unicode-bom': "error"
+    'eol-last': ['error', 'always'],
+    'no-mixed-spaces-and-tabs': 'error',
+    'no-multi-spaces': ['error', { ignoreEOLComments: true }],
+    'no-multiple-empty-lines': ['error', { max: 1, maxBOF: 0, maxEOF: 0}],
+    'no-tabs': 'error',
+    'no-trailing-spaces': 'error',
+    'unicode-bom': 'error'
   }
 }
